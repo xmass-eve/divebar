d939ce464677f08f18e5cec40f81df6f8e8feeee
xmass-eve <shoppingeve81@gmail.com>
2017-07-30 17:27:59 +0200

    template basic

--- tqtabootstrap.js
+++ tqtabootstrap.js
@@ -1,20 +1,35 @@
+// $(document).ready(function(){
+//     showBlog('#indocontent');
+// });
 
+// var blogContents = ["#indocontent", "#gilicontent", "#lembongancontent", "#balicontent", "#lombokcontent", "#komodocontent", "#rajacontent", "#lembehcontent", "#bunakencontent", "#othercontent"];
+// function showBlog(blog)
+// {
+//     for (var i = 0;i < blogContents.length;i++)
+//     {
+//         $(blogContents[i]).css("display", "none");
+//     }
 
-$(document).ready(function(){
-    
-});
+//     $(blog).css("display", "block");
+// }
 
-var blogContents = ["#indocontent", "#gilicontent", "#lembongancontent", "#balicontent", "#lombokcontent", "#komodocontent", "#rajacontent", "#lembehcontent", "#bunakencontent", "#othercontent"];
-function showBlog(blog)
-{
-    for (var i = 0;i < blogContents.length;i++)
-    {
-        $(blogContents[i]).css("display", "none");
-    }
-
-    $(blog).css("display", "block");
+function showPage(page) {
+    $("#pagecontent").load(page);
 }
 
-  
+function showMenu(menu) {
+    $("#pagemenu").load(menu);
+}
 
- 
+function setBanner(url) {
+    $("#banner").css("background-image", "url('" + url + "')");
+}
+
+function setHeader(header) {
+    $("#bannertitle").text(header);
+}
+
+
+
+
+
