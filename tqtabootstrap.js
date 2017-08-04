7bacb85dd96efdb8036901b321e7711db9da3133
xmass-eve <shoppingeve81@gmail.com>
2017-08-04 15:48:57 -0600

    change template and write new folders

--- tqtabootstrap.js
+++ tqtabootstrap.js
@@ -1,17 +1,4 @@
-// $(document).ready(function(){
-//     showBlog('#indocontent');
-// });
 
-// var blogContents = ["#indocontent", "#gilicontent", "#lembongancontent", "#balicontent", "#lombokcontent", "#komodocontent", "#rajacontent", "#lembehcontent", "#bunakencontent", "#othercontent"];
-// function showBlog(blog)
-// {
-//     for (var i = 0;i < blogContents.length;i++)
-//     {
-//         $(blogContents[i]).css("display", "none");
-//     }
-
-//     $(blog).css("display", "block");
-// }
 
 function showPage(page) {
     $("#pagecontent").load(page);
